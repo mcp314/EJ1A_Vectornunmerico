3d1efce2bda2acea892192167a12775a2dab0bc9
Gaby <Gaby@DESKTOP-1VQNKUS>
2022-08-23 00:15:01 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vector;
+
+namespace EJ1A_Vectornunmerico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = new VectorNumerico();
+            if (v.Validar())
+            {
+                v.GenerarVector();
+
+                Console.WriteLine(v.ToString());//imprimir en una linea
+            }
+            else
+            {
+                Console.WriteLine("Objeto no válido");
+            }
+
+            Console.WriteLine("convierto a string:");
+            Console.WriteLine("");
+            //var nuevo = new VectorNumerico;
+            //nuevo=4*v;
+            //Console.WriteLine(nuevo.ToString());
+
+            Console.ReadLine();
+        }
+    }
+    
+}
